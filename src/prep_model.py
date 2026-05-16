403664838ae8c947be098aa512120eff3cee679f
Loris Polenz <contact@lorispolenz.com>
2026-05-16 21:43:02 +0200

    feat(prep_model): add arguments for model and version selection

--- src/prep_model.py
+++ src/prep_model.py
@@ -1,4 +1,5 @@
 import mlflow
+import argparse
 from pathlib import Path
 from mlflow.tracking import MlflowClient
 from optimum.exporters.onnx import main_export
@@ -10,9 +11,18 @@ client = MlflowClient()
 
 MODEL_PATH = "models/distilbert/"
 
+parser = argparse.ArgumentParser(
+    prog='Prep Model',
+    description='Pepare Model for usage in API. Downloads model and exports it into onnx format.'
+)
+
+parser.add_argument("-m", '--model', required=True)
+parser.add_argument("-v", '--version', required=True)
+
+args = parser.parse_args()
+
 base_uri = client.get_model_version_download_uri(
-    "distilbert-finetuned",
-    version="12"
+    args.model, version=args.version
 )
 
 print(f"Downloading model from: {base_uri}")
